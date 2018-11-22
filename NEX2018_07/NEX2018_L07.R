4fb455302245086fa0f4a87314980282e941708d
belov <vladislav.belov@merck.com>
2018-11-22 13:48:02 +0100

    lecture 8 added

--- NEX2018_07/NEX2018_L07.R
+++ NEX2018_07/NEX2018_L07.R
@@ -1,197 +1,191 @@
 # NEX 2017 - R codes used in Lecture 07
 # Written by J. Franc - jiri.franc@fjfi.cvut.cz
 # Some parts of code is from R Companion to Montgomery?s DAoE
-# http://www.aliquote.org/articles/tech/dae/ 
+# http://www.aliquote.org/articles/tech/dae/
 
-#Instalation of any library
-library(car)          # provides a set of useful functions for ANOVA designs and Regression Models;
-library(lattice)      # provides some graphical enhancements compared to traditional R graphics, as well as multivariate displays capabilities;
-library(lme4)         # the newer and enhanced version of the nlme package, for which additional data structure are available (nested or hierarchical model,. . . );
-library(nlme)         # for handling mixed-effects models;
-library(pwr)          # power analysis
-library(agricolae)    # for Fisher LSD method
-library(scatterplot3d)# for 3d scatter plot
+# Instalation of any library
+library(car) # provides a set of useful functions for ANOVA designs and Regression Models;
+library(lattice) # provides some graphical enhancements compared to traditional R graphics, as well as multivariate displays capabilities;
+library(lme4) # the newer and enhanced version of the nlme package, for which additional data structure are available (nested or hierarchical model,. . . );
+library(nlme) # for handling mixed-effects models;
+library(pwr) # power analysis
+library(agricolae) # for Fisher LSD method
+library(scatterplot3d) # for 3d scatter plot
 library(alr3)
-library(DoE.base)      # Full factorials, orthogonal arrays and base utilities for DoE packages
-library(rsm)           # Response-Surface Methods
+library(DoE.base) # Full factorials, orthogonal arrays and base utilities for DoE packages
+library(rsm) # Response-Surface Methods
 library(qualityTools)
 # for opening xls files: library(gdata) library(XLConnect) library(xlsReadWrite)
-
+library(FrF2) # for 2^k  factorial design
 
 
 # Define directory
-setwd("D:/Vyuka/NEX/Lecture_07/")
-library(FrF2)          #for 2^k  factorial design
+setwd(paste0(getwd(), "/NEX2018_07"))
 
 ##############################################################################
 # Follow Chapter 6 - Montgomery DAOE
 ##############################################################################
 
 ##############################################################################
-### Single Replicate of the 2^4 design 
+### Single Replicate of the 2^4 design
 ##############################################################################
 
 
-rate_experiment             <- read.table("Pilot_Plant_Filtration_Rate_Experiment.csv",header=TRUE, sep = ";")
+rate_experiment <- read.table("Pilot_Plant_Filtration_Rate_Experiment.csv", header = TRUE, sep = ";")
 summary(rate_experiment)
-rate_experiment$A           <- factor(rate_experiment$A) 
-rate_experiment$B           <- factor(rate_experiment$B) 
-rate_experiment$C           <- factor(rate_experiment$C)
-rate_experiment$D           <- factor(rate_experiment$D) 
+rate_experiment$A <- factor(rate_experiment$A)
+rate_experiment$B <- factor(rate_experiment$B)
+rate_experiment$C <- factor(rate_experiment$C)
+rate_experiment$D <- factor(rate_experiment$D)
 summary(rate_experiment)
 
-#rate           <-  FrF2(2^4, 4, replications = 1, randomize = FALSE,factor.names = c("A_Temperature", "B_Pressure", "C_Concentration", "D_Stirring_rate"))
-rate            <-  FrF2(2^4, 4, replications = 1, randomize = FALSE,factor.names = c("A", "B", "C", "D"))
-Filtration      <-  rate_experiment$Rate
-rate            <-  add.response(rate, Filtration)
+# rate           <-  FrF2(2^4, 4, replications = 1, randomize = FALSE,factor.names = c("A_Temperature", "B_Pressure", "C_Concentration", "D_Stirring_rate"))
+rate <- FrF2(2^4, 4, replications = 1, randomize = FALSE, factor.names = c("A", "B", "C", "D"))
+Filtration <- rate_experiment$Rate
+rate <- add.response(rate, Filtration)
 summary(rate)
 # Main Effects plotfor response variable
 MEPlot(rate)
 # Interaction Plot matrix for response variable
 IAPlot(rate)
-#Boxplots
-opar <- par(mfrow=c(2,2))
-with(rate, boxplot(Filtration ~ A , main = "Factor A - Temperature") )
-with(rate, boxplot(Filtration ~ B , main = "Factor B - Pressure")     )
-with(rate, boxplot(Filtration ~ C , main = "Factor C - Concentration") )
-with(rate, boxplot(Filtration ~ D , main = "Factor D - Stirring rate")  )
-title(main="Box plots of all factors", line=-1, outer=T, cex.main=1)
-par(opar)    
+# Boxplots
+opar <- par(mfrow = c(2, 2))
+with(rate, boxplot(Filtration ~ A, main = "Factor A - Temperature"))
+with(rate, boxplot(Filtration ~ B, main = "Factor B - Pressure"))
+with(rate, boxplot(Filtration ~ C, main = "Factor C - Concentration"))
+with(rate, boxplot(Filtration ~ D, main = "Factor D - Stirring rate"))
+title(main = "Box plots of all factors", line = -1, outer = T, cex.main = 1)
+par(opar)
 
-opar <- par(mfrow=c(1,1))
+opar <- par(mfrow = c(1, 1))
 ## Daniel plot and Half normal plot are usually used for unreplicatd design
 # Daniel Plot with alpha = 0.1 and only significant factors
-DanielPlot(rate,code=TRUE)
+DanielPlot(rate, code = TRUE)
 # Classical effects qqplot
-qqplot(DanielPlot(rate,alpha=0.1)$x,DanielPlot(rate)$y)
-qqline(DanielPlot(rate,alpha=0.1)$y)
+qqplot(DanielPlot(rate, alpha = 0.1)$x, DanielPlot(rate)$y)
+qqline(DanielPlot(rate, alpha = 0.1)$y)
 # half normal plot of effects
-DanielPlot(rate,code=TRUE,alpha=0.1,half=TRUE)
+DanielPlot(rate, code = TRUE, alpha = 0.1, half = TRUE)
 
 
 
 
 # B not significant at SL 0.1 - discard B and have 2^3 design with two replicates
-rate2            <-  FrF2(2^3, 3, replications = 2, randomize = FALSE,factor.names = c("A", "C", "D"),repeat.only=TRUE)
-Filtration2      <-  c(45 , 48 , 71 , 65 , 68 ,  80 , 60 , 65 , 43 , 45 , 100 , 104 , 75 , 70 , 86 , 96)
-rate2            <-  add.response(rate2, Filtration2)
-anova(aov(Filtration2~A*C*D, data=rate2))
+rate2 <- FrF2(2^3, 3, replications = 2, randomize = FALSE, factor.names = c("A", "C", "D"), repeat.only = TRUE)
+Filtration2 <- c(45, 48, 71, 65, 68, 80, 60, 65, 43, 45, 100, 104, 75, 70, 86, 96)
+rate2 <- add.response(rate2, Filtration2)
+anova(aov(Filtration2 ~ A * C * D, data = rate2))
 # same as original linear model without factor B
-anova(aov(Filtration~A*C*D, data=rate))
+anova(aov(Filtration ~ A * C * D, data = rate))
 
 # final model
-anova(aov(Filtration~A*C+A*D,  data=rate))
-model1 = lm(Filtration~A+C+D+A:C+A:D,  data=rate)
+anova(aov(Filtration ~ A * C + A * D, data = rate))
+model1 <- lm(Filtration ~ A + C + D + A:C + A:D, data = rate)
 summary(model1)
 
 
 # another approach
-# model with all factors and all interactions - we do not have "pure error" 
-anova(aov(Filtration~(.)^4,  data=rate))
-# model with all factors and all interactions - we omit the highest interaction 
-anova(aov(Filtration~(.)^3,  data=rate))
+# model with all factors and all interactions - we do not have "pure error"
+anova(aov(Filtration ~ (.)^4, data = rate))
+# model with all factors and all interactions - we omit the highest interaction
+anova(aov(Filtration ~ (.)^3, data = rate))
 # use only significant variables
-anova(aov(Filtration~A+C+D+A:C+A:D,  data=rate))
-model1 = lm(Filtration~A+C+D+A:C+A:D,  data=rate)
+anova(aov(Filtration ~ A + C + D + A:C + A:D, data = rate))
+model1 <- lm(Filtration ~ A + C + D + A:C + A:D, data = rate)
 summary(model1)
 
-opar <- par(mfrow=c(2,2))
+opar <- par(mfrow = c(2, 2))
 plot(model1)
 par(opar)
 
 
-#Take as a numeric -1, +1 instead of factor
-rate$A.num <- 2*(as.numeric(rate$A)-1.5)
-rate$C.num <- 2*(as.numeric(rate$C)-1.5)
-rate$D.num <- 2*(as.numeric(rate$D)-1.5)
-rate.lm       <- lm(Filtration~A.num*C.num+A.num*D.num,data=rate)
+# Take as a numeric -1, +1 instead of factor
+rate$A.num <- 2 * (as.numeric(rate$A) - 1.5)
+rate$C.num <- 2 * (as.numeric(rate$C) - 1.5)
+rate$D.num <- 2 * (as.numeric(rate$D) - 1.5)
+rate.lm <- lm(Filtration ~ A.num * C.num + A.num * D.num, data = rate)
 summary(rate.lm)
-tmp           <- list(A.num=seq(-1,1,by=.05),C.num=seq(-1,1,by=0.05),D.num=seq(-1,1,by=0.05),data=rate)
+tmp <- list(A.num = seq(-1, 1, by = .05), C.num = seq(-1, 1, by = 0.05), D.num = seq(-1, 1, by = 0.05), data = rate)
 
 
-new.data2 <- t(rbind(seq(-1,1,by=.05),seq(-1,1,by=.05),seq(-1,1,by=.05)))
-colnames(new.data2) <-c("A.num","C.num","D.num")
+new.data2 <- t(rbind(seq(-1, 1, by = .05), seq(-1, 1, by = .05), seq(-1, 1, by = .05)))
+colnames(new.data2) <- c("A.num", "C.num", "D.num")
 new.data2 <- as.data.frame(new.data2)
 new.data2 <- expand.grid(new.data2)
 
-new.data       <- expand.grid(tmp)
-new.data$fit   <- predict(rate.lm,new.data)
-new.data$fit2  <- predict(rate.lm,new.data2)
+new.data <- expand.grid(tmp)
+new.data$fit <- predict(rate.lm, new.data)
+new.data$fit2 <- predict(rate.lm, new.data2)
 
 # Countour plots - only 2 variables in the model
-contourplot(fit~A.num*C.num,new.data,xlab="A Temperature",ylab="C Concentration", main = "Contour plot of Filtration Rate from the Pilot Plant Experiment")
-contourplot(fit~C.num*D.num,new.data,xlab="C Concentration",ylab="D Stiring Rate", main = "Contour plot of Filtration Rate from the Pilot Plant Experiment")
-contourplot(fit~A.num*D.num,new.data,xlab="A Temperature",ylab="D Stiring Rate", main = "Contour plot of Filtration Rate from the Pilot Plant Experiment")
+contourplot(fit ~ A.num * C.num, new.data, xlab = "A Temperature", ylab = "C Concentration", main = "Contour plot of Filtration Rate from the Pilot Plant Experiment")
+contourplot(fit ~ C.num * D.num, new.data, xlab = "C Concentration", ylab = "D Stiring Rate", main = "Contour plot of Filtration Rate from the Pilot Plant Experiment")
+contourplot(fit ~ A.num * D.num, new.data, xlab = "A Temperature", ylab = "D Stiring Rate", main = "Contour plot of Filtration Rate from the Pilot Plant Experiment")
 
 # Countour plots - all 3 varaibles + interactions in the model
-par(mfrow = c(2,2))
-wirePlot(A, C, Filtration_qt, data = rate_qt,form = "Filtration_qt~A*C+A*D")
-contourPlot(A, C, Filtration_qt, data = rate_qt,form = "Filtration_qt~A*C+A*D")
-contourPlot(C, D, Filtration_qt, data = rate_qt,form = "Filtration_qt~A*C+A*D")
-contourPlot(A, D, Filtration_qt, data = rate_qt,form = "Filtration_qt~A*C+A*D")
+par(mfrow = c(2, 2))
+wirePlot(A, C, Filtration_qt, data = rate_qt, form = "Filtration_qt~A*C+A*D")
+contourPlot(A, C, Filtration_qt, data = rate_qt, form = "Filtration_qt~A*C+A*D")
+contourPlot(C, D, Filtration_qt, data = rate_qt, form = "Filtration_qt~A*C+A*D")
+contourPlot(A, D, Filtration_qt, data = rate_qt, form = "Filtration_qt~A*C+A*D")
 
-rate0             <- read.table("Pilot_Plant_Filtration_Rate_Experiment.csv",header=TRUE, sep = ";")
-rate3             <- read.table("Pilot_Plant_Filtration_Rate_Experiment3.csv",header=TRUE, sep = ";")
+rate0 <- read.table("Pilot_Plant_Filtration_Rate_Experiment.csv", header = TRUE, sep = ";")
+rate3 <- read.table("Pilot_Plant_Filtration_Rate_Experiment3.csv", header = TRUE, sep = ";")
 
 
 summary(rate3)
-anova(aov(Rate~A*B*C*D,data=rate3))
-rate3$E <- c(rep(0,times=16),rep(1,times=4))
-aov_model1= aov(Rate~A*B*C*D,data=rate3)
+anova(aov(Rate ~ A * B * C * D, data = rate3))
+rate3$E <- c(rep(0, times = 16), rep(1, times = 4))
+aov_model1 <- aov(Rate ~ A * B * C * D, data = rate3)
 summary(aov_model1)
-aov_model2= aov(Rate~A*B*C*D+E,data=rate3)
+aov_model2 <- aov(Rate ~ A * B * C * D + E, data = rate3)
 summary(aov_model2)
-#anova(aov(Rate~as.factor(A)*as.factor(B)*as.factor(C)*as.factor(D)+as.factor(E),data=rate3))
+# anova(aov(Rate~as.factor(A)*as.factor(B)*as.factor(C)*as.factor(D)+as.factor(E),data=rate3))
 
-anova(aov(Rate~A*B*C*D,data=rate0))
+anova(aov(Rate ~ A * B * C * D, data = rate0))
 
-aov_model1= aov(Rate~A*B*C*D,data=rate3)
+aov_model1 <- aov(Rate ~ A * B * C * D, data = rate3)
 model.tables(aov_model1, "means")
 
-anova(aov(Filtration~A*B*C*D,data=rate))
+anova(aov(Filtration ~ A * B * C * D, data = rate))
 
-anova(aov(Rate~A*C+A*D,data=rate3))
-
-
-rate4            <-  FrF2(2^3, 3, replications = 2, randomize = FALSE,factor.names = c("A", "C", "D"),repeat.only=TRUE)
-rate4            <-  add.center(rate4,2)
-Filtration4      <-  c(45 , 48 , 71 , 65 , 68 ,  80 , 60 , 65 , 43 , 45 , 100 , 104 , 75 , 70 , 86 , 96, 73,75,66,69)
-rate4            <-  add.response(rate4, Filtration4)
-anova(aov(Filtration2~A*C+A*D,  data=rate2))
-anova(aov(Filtration4~A*C+A*D,  data=rate4))
-
-summary(lm(Filtration4~A*C+A*D+iscube(rate4), rate4))
-
-#195.13 + 48.75 + 1.51
-#245.39
+anova(aov(Rate ~ A * C + A * D, data = rate3))
 
 
+rate4 <- FrF2(2^3, 3, replications = 2, randomize = FALSE, factor.names = c("A", "C", "D"), repeat.only = TRUE)
+rate4 <- add.center(rate4, 2)
+Filtration4 <- c(45, 48, 71, 65, 68, 80, 60, 65, 43, 45, 100, 104, 75, 70, 86, 96, 73, 75, 66, 69)
+rate4 <- add.response(rate4, Filtration4)
+anova(aov(Filtration2 ~ A * C + A * D, data = rate2))
+anova(aov(Filtration4 ~ A * C + A * D, data = rate4))
 
+summary(lm(Filtration4 ~ A * C + A * D + iscube(rate4), rate4))
 
+# 195.13 + 48.75 + 1.51
+# 245.39
 
 
 # Pareto plot
-rate_effects  <-  DanielPlot(rate,alpha=0.1)$x
+rate_effects <- DanielPlot(rate, alpha = 0.1)$x
 
-source("pareto.R")  
-pareto(rate_effects, names = c("A","B","C","D","AB","AC","AD","BC","BD","CD","ABC","ABD","ACD","BCD","ABCD"))
+source("pareto.R")
+pareto(rate_effects, names = c("A", "B", "C", "D", "AB", "AC", "AD", "BC", "BD", "CD", "ABC", "ABD", "ACD", "BCD", "ABCD"))
 
-rate_qt = fracDesign(k = 4, replicates = 1,random.seed     = 0)  
-Filtration_qt= rate$Filtration[rate_qt[,1]]
-response(rate_qt) = Filtration_qt 
-opar = par(mfrow = c(1,2))
+rate_qt <- fracDesign(k = 4, replicates = 1, random.seed = 0)
+Filtration_qt <- rate$Filtration[rate_qt[, 1]]
+response(rate_qt) <- Filtration_qt
+opar <- par(mfrow = c(1, 2))
 paretoPlot(rate_qt)
 normalPlot(rate_qt)
 par(opar)
 
 
 
-Problem_06_26    <- read.table("Ex06_26.csv",header=TRUE, sep = ";")
-Ex26             <-  FrF2(2^5, 5, replications = 1, randomize = FALSE,
-                     factor.names = c("A", "C", "D","E","F"),repeat.only=TRUE,ncenter=4)
-Yield            <-  Problem_06_26$Yield
-Ex26             <-  add.response(Ex26, Yield)
+Problem_06_26 <- read.table("Ex06_26.csv", header = TRUE, sep = ";")
+Ex26 <- FrF2(2^5, 5,
+  replications = 1, randomize = FALSE,
+  factor.names = c("A", "C", "D", "E", "F"), repeat.only = TRUE, ncenter = 4
+)
+Yield <- Problem_06_26$Yield
+Ex26 <- add.response(Ex26, Yield)
 summary(Ex26)
-
-
-
